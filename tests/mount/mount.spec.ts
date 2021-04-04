aa7998f81cab8dfb480468f264906693b786b9f1
Carlos A. Zequeira Sánchez <zequeira@xetid.cu>
2021-04-03 23:20:57 -0400

    configurado jsdom global

--- tests/mount/mount.spec.ts
+++ tests/mount/mount.spec.ts
@@ -1,8 +1,17 @@
+import jsdomGlobal from 'jsdom-global';
 import { JSDOM } from 'jsdom';
 
+jsdomGlobal();
+
 test('test', () => {
-  const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
-  expect(dom.window.document.querySelector('p')?.textContent).toBe(
-    'Hello world',
-  );
+  const script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.text = `document.querySelector('p').innerHTML = 'HOLA'`;
+
+  const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`, {
+    runScripts: 'dangerously',
+  });
+  dom.window.document.body.appendChild(script);
+
+  expect(dom.window.document.querySelector('p')?.textContent).toBe('HOLA');
 });
