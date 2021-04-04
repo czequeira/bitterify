64d4b9f90e82050c045395a55afe398d8fdcee9f
Carlos A. Zequeira Sánchez <zequeira@xetid.cu>
2021-04-03 23:34:04 -0400

    creado el core

---
+++ src/core/index.ts
@@ -0,0 +1,10 @@
+import { App, Component } from '../classes';
+import { Content } from '../types';
+
+export function createApp(...components: Component[]): App {
+  return new App(components);
+}
+
+export function createComponent(htmlType: string, content: Content): Component {
+  return new Component(htmlType, content);
+}
