eb88188f1066b4447269801c1e13189b0f1890f1
Carlos A. Zequeira Sánchez <zequeira@xetid.cu>
2021-04-04 01:00:51 -0400

    Creada la primera test

--- tests/mount/mount.spec.ts
+++ tests/mount/mount.spec.ts
@@ -1,17 +1,39 @@
 import jsdomGlobal from 'jsdom-global';
 import { JSDOM } from 'jsdom';
+import browserify from 'browserify';
+import tsify from 'tsify';
 
 jsdomGlobal();
 
-test('test', () => {
+test('test', async (done) => {
   const script = document.createElement('script');
   script.type = 'text/javascript';
-  script.text = `document.querySelector('p').innerHTML = 'HOLA'`;
+  // script.text = `document.querySelector('p').innerHTML = 'HOLA'`;
 
-  const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`, {
-    runScripts: 'dangerously',
-  });
-  dom.window.document.body.appendChild(script);
+  browserify()
+    .add('tests/static/requires.ts')
+    .plugin(tsify)
+    .bundle(function (err, buf) {
+      if (err) console.error(err);
+      const compiled = buf.toString();
+      const index = compiled.search('¿');
 
-  expect(dom.window.document.querySelector('p')?.textContent).toBe('HOLA');
+      const compiledBegin = compiled.substring(0, index);
+      const compiledEnd = compiled.substring(index + 1);
+      const code = `
+      const n = createComponent('p', 'Hello world');
+      createApp(n);
+      `;
+      script.text = compiledBegin + code + compiledEnd;
+
+      const dom = new JSDOM(`<!DOCTYPE html><div id="app" />`, {
+        runScripts: 'dangerously',
+      });
+      dom.window.document.body.appendChild(script);
+
+      expect(dom.window.document.querySelector('p')?.innerText).toBe(
+        'Hello world',
+      );
+      done();
+    });
 });
---
+++ tests/static/requires.ts
@@ -0,0 +1,9 @@
+import {
+  createApp as createAppO,
+  createComponent as createComponentO,
+} from '../../src/core';
+
+const createComponent = createComponentO;
+const createApp = createAppO;
+
+// inject code here ¿
