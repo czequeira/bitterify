f2e2c17f592d046bf178294b9373a59a349b92db
Carlos A. Zequeira Sánchez <zequeira@xetid.cu>
2021-04-30 13:55:15 -0400

    añadido el componente header

--- src/components/layout.component.ts
+++ src/components/layout.component.ts
@@ -1,6 +1,7 @@
 import { createDinamicChilds } from './utils';
 
 export const aside = createDinamicChilds('aside');
+export const header = createDinamicChilds('header');
 export const nav = createDinamicChilds('nav');
 export const footer = createDinamicChilds('footer');
 export const main = createDinamicChilds('main');
